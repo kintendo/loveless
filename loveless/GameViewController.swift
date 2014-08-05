9076f0b315219df4d6c74e8d797febe6966e5c72
kintendo <kinthai@gmail.com>
2014-08-04 20:48:36 -0700

    this is a test commit -kin

--- loveless/GameViewController.swift
+++ loveless/GameViewController.swift
@@ -60,6 +60,7 @@ class GameViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
+        // This is a test comment -Kin
     }
     
 }
