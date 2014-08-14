da12a3e557b5e176eb136e6d9137d224665dcd66
kintendo <kinthai@gmail.com>
2014-08-13 22:03:41 -0700

    grass on screen

--- loveless/GameScene.swift
+++ loveless/GameScene.swift
@@ -9,33 +9,28 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    let tile = SKSpriteNode(imageNamed:"grass")
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        //tile.size = CGSizeMake(CGFloat(20), CGFloat(5))
         
-        self.addChild(myLabel)
+//        var grid: SKSpriteNode[][] = [[self.tile, self.tile],[self.tile, self.tile]]
+                var grid: SKSpriteNode[][] = [[self.tile],[self.tile]]
+
+        for row in grid {
+            for item in row {
+                item.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame))
+                self.addChild(item)
+            }
+        }
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
         for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            //do nothing for now
         }
     }
    
