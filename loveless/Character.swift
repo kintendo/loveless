d80c6c846c64bc99ef387975162a3efe5e4d1e98
Corey Casado <OryoCascade@Coreys-MacBook-Pro.local>
2014-08-28 22:46:43 -0700

    updates for character class

---
+++ loveless/Character.swift
@@ -0,0 +1,13 @@
+//
+//  Character.swift
+//  loveless
+//
+//  Created by Corey Casado on 8/28/14.
+//  Copyright (c) 2014 Kinetic Core. All rights reserved.
+//
+
+import SpriteKit
+
+class Character {
+    
+}
